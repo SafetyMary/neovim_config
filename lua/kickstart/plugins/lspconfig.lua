c36eb64c5df1b2c54a9ef9b1208ee4207f511906
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-08 11:47:09 +0800

    add pyright and basedpyright

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -273,6 +273,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'pyright',
+        'basedpyright',
         'pylsp',
         'ruff',
         'sqlfluff',
